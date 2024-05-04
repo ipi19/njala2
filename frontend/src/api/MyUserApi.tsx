a4eb1dce2f9ae609f445f8ddbc7521889fbe4df1
ipi19 <ipyanamwakikunga@gmail.com>
2024-05-04 12:25:09 +0200

    jhdfk

---
+++ frontend/src/api/MyUserApi.tsx
@@ -0,0 +1,38 @@
+import { useMutation } from "react-query";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+type createUserRequest = {
+  auth0Id: string;
+  email: string;
+};
+
+export const userCreateMyUser = () => {
+  const createMyUserRequest = async (user: createUserRequest) => {
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    if (!response.ok) {
+      throw new Error("Filed to create user!");
+    }
+  };
+
+  const {
+    mutateAsync: createUser,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useMutation(createMyUserRequest);
+
+  return {
+    createUser,
+    isLoading,
+    isError,
+    isSuccess,
+  };
+};
